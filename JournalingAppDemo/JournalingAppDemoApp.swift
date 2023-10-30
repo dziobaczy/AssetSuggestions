0063275c5dcc0a247e449436b3a0a0ff73783234
Piotr Szadkowski <offthecode@gmail.com>
2023-10-30 18:56:08 +0100

    Initial Commit

---
+++ JournalingAppDemo/JournalingAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalingAppDemoApp.swift
+//  JournalingAppDemo
+//
+//  Created by Piotr Szadkowski on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct JournalingAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
